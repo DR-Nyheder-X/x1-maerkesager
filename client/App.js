20d978c1e24838d70f5a49f6c311ca132ce4778a
Mikkel Malmberg <mikkel@brnbw.com>
2015-06-02 14:55:34 +0200

    ..

--- client/App.js
+++ client/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 
-if (process.env.NODE_ENV === 'development') {
-  require('./App.scss')
-}
+require('./App.scss')
 
 export default class App extends React.Component {
   render() {
